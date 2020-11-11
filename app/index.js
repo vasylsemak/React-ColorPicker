9cc66c446a36707c96d09fa3b77b76fadbd57695
Vasyl Semak <vasylsmk1401@gmail.com>
2020-11-10 20:56:39 -0500

    create handleChange method

--- app/index.js
+++ app/index.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom';
 import Color from '../components/Color.js';
 
 class Picker extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      selectedColor: "green"
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(color) {
+    this.setState({selectedColor: color});
+  }
+
   render() {
     return (
       <div id="container">
         <div id="navbar">
           <div>Currently selected: </div>
-          <div className="red">red</div>
+          <div className={this.state.selectedColor}>{this.state.selectedColor}</div>
         </div>
         <div id="colors-list">
-          <Color />
-          <Color />
-          <Color />
+          <Color color="red" selectColor={this.handleChange} />
+          <Color color="blue" selectColor={this.handleChange} />
+          <Color color="green" selectColor={this.handleChange} />
         </div>
       </div>
     );
--- components/Color.js
+++ components/Color.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export default () => {
-  return ( <div className="red" /> )
+export default ({ color, selectColor }) => {
+  return (
+    <div className={color} onClick={() => selectColor(color)} />
+  )
 }
